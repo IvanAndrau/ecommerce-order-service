17d953873b3b93d07d3ef5dd5f575e600a90eba2
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-03-15 19:33:33 +0100

    feat(exception): Define ErrorDetails class for structured error responses - Created ErrorDetails class to standardize error responses. - Includes timestamp, message, and details for better debugging.

---
+++ ecommerce-order-service/src/main/java/com/example/ecommerce_order_service/exceptions/ErrorDetails.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce_order_service.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+}
