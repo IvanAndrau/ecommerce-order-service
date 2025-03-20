f5954d48deedc38e0dc1a9fd5e6c6ab63a0200b2
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-03-20 08:08:55 +0100

    feat(docs): Enable JWT authentication in Swagger UI
    - Added security scheme for JWT-based authentication

--- ecommerce-order-service/src/main/java/com/example/ecommerce_order_service/config/SwaggerConfig.java
+++ ecommerce-order-service/src/main/java/com/example/ecommerce_order_service/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,6 +26,18 @@ public class SwaggerConfig {
                 )
                 .externalDocs(new ExternalDocumentation()
                 .description("Project GitHub Repository")
-                .url("https://github.com/IvanAndrau/ecommerce-order-service"));
+                .url("https://github.com/IvanAndrau/ecommerce-order-service"))
+
+                // Enable JWT Security
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter your JWT token here")
+                        )
+                );
     }
 }
